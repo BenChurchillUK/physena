e0b9f3c297078b4945fb3e42bb43074317a84c6c
Ben Churchill <benjamin.churchill.uk@gmail.com>
2026-09-11 13:16:13 +0100

    Initial Setup
    The initial setting up of the nameless library. This isn't meant to be a large project, just one to see how packages work.

---
+++ src/Physics/core/constants.py
@@ -0,0 +1,14 @@
+c = 3e8  # ms^-1
+
+#  Astrophysics -- Solar Units
+
+solar_luminosity = 3.846e26  # W
+solar_mass = 1.989e30  # kg
+
+#  Particles
+
+atomic_mass_unit = 1.661e-27  # kg
+
+#  Thermodynamics
+
+molar_gas_constant = 8.315 #  J mol-1 K-1
